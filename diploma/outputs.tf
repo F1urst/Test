e2dff6402b3d949e3725949df2050cc7efd5b57d
F1urst <dimkorm@gmail.com>
2026-09-15 08:31:35 +0300

    Update outputs.tf

--- diploma/outputs.tf
+++ diploma/outputs.tf
@@ -1,27 +1,31 @@
+output "alb_ip" {
+  description = "Public IP of Application Load Balancer"
+  value       = yandex_alb_load_balancer.web.listener[0].endpoint[0].address[0].external_ipv4_address[0].address
+}
+
 output "bastion_ip" {
-  value = yandex_compute_instance.bastion.network_interface[0].nat_ip_address
+  description = "Public IP of bastion host"
+  value       = yandex_compute_instance.bastion.network_interface[0].nat_ip_address
 }
 
 output "zabbix_ip" {
-  value = yandex_compute_instance.zabbix.network_interface[0].nat_ip_address
+  description = "Public IP of Zabbix"
+  value       = yandex_compute_instance.zabbix.network_interface[0].nat_ip_address
 }
 
 output "kibana_ip" {
-  value = yandex_compute_instance.kibana.network_interface[0].nat_ip_address
+  description = "Public IP of Kibana"
+  value       = yandex_compute_instance.kibana.network_interface[0].nat_ip_address
 }
 
-output "web_1_private_ip" {
-  value = yandex_compute_instance.web-1.network_interface[0].ip_address
-}
-
-output "web_2_private_ip" {
-  value = yandex_compute_instance.web-2.network_interface[0].ip_address
+output "web_servers" {
+  description = "Web servers internal IPs"
+  value = {
+    for k, v in yandex_compute_instance.web : k => v.network_interface[0].ip_address
+  }
 }
 
 output "elastic_private_ip" {
-  value = yandex_compute_instance.elastic.network_interface[0].ip_address
-}
-
-output "load_balancer_ip" {
-  value = yandex_alb_load_balancer.web-alb.listener[0].endpoint[0].address[0].external_ipv4_address[0].address
+  description = "Elasticsearch internal IP"
+  value       = yandex_compute_instance.elastic.network_interface[0].ip_address
 }
